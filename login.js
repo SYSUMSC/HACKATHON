29921e88351e89dc622a330b10cf9641c1e4fc26
hatut <hatut@foxmail.com>
2020-03-16 19:40:45 +0800

    登录界面+导航

---
+++ login.js
@@ -0,0 +1,28 @@
+function foLogin() {
+    var oUname = document.getElementById("uname");
+    var oError = document.getElementById("error_box");
+    var oUpsd = document.getElementById("upsd");
+
+    var isError = true;
+    oError.innerHTML = "<br>";
+
+    if (oUname.value.length < 6 || oUname.value.length > 12) {
+        oError.innerHTML = "*无该用户名信息*";
+        isError = false;
+        return;
+    }else if(oUname.value.charCodeAt(0)>=48 &&(oUname.value.charCodeAt(0)<=57)){
+        oError.innerHTML="*无该用户名信息*";
+        return;
+
+    }else for (var i=0;i<oUname.value.length;i++){
+       if((oUname.value.charCodeAt(i)<48)||(oUname.value.charCodeAt(i)>57)&&(oUname.value.charCodeAt(i)<58)&&(oUname.value.charCodeAt(i)>97)){
+        oError.innerHTML="*无该用户名信息*";
+        return;
+    }
+    }
+    if (oUpsd.value.length < 6 ) {
+        oError.innerHTML = "*密码大于6位*";
+        isError = false;
+        return;
+    }
+}
